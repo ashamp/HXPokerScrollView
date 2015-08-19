000b572d29898b29e7b56e30310a9b1d4f5d4931
谢俊伟 <getfilebox@gmail.com>
2015-08-19 09:12:11 +0800

    init

---
+++ HXPokerScrollView/HXPokerScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HXPokerScrollView
+//
+//  Created by MacBook on 15/8/19.
+//  Copyright (c) 2015年 MacBook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
